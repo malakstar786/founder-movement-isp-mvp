f796affe66ec35fe10acb5f954c4eadbebda5191
Hussain Personal <bedawalahussain553@gmail.com>
2025-05-20 10:23:28 +0300

    Updates rapid

--- pages/4_Settings.py
+++ pages/4_Settings.py
@@ -1,5 +1,11 @@
-import streamlit as st
+import sys
 import os
+# Add the project root to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+import streamlit as st
 import json
 from dotenv import load_dotenv
 import tempfile
